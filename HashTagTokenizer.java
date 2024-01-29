ca147a6837122ac4dc7baae93242f6119e9d7f64
loren.kaso <loren.kaso@post.runi.ac.il>
2024-01-29 18:39:06 +0200

    first

--- HashTagTokenizer.java
+++ HashTagTokenizer.java
@@ -13,14 +13,20 @@ public class HashTagTokenizer {
 		String[] dictionary = new String[3000];
 
 		In in = new In(fileName);
-
-		// Your code here
-
+		while (!in.isEmpty()) {
+			int i = in.readInt();
+			dictionary[i] = in.readString();
+		}
 		return dictionary;
 	}
 
 	public static boolean existInDictionary(String word, String []dictionary) {
-		// Your code here
+		for (int i = 0; i < dictionary.length ; i++) {
+			if(dictionary[i] == word){
+				return true;
+			}
+		}
+		return false;
 	}
 
 	public static void breakHashTag(String hashtag, String[] dictionary) {
@@ -31,9 +37,12 @@ public class HashTagTokenizer {
         }
  
         int N = hashtag.length();
-
-        for (int i = 1; i <= N; i++) {
 		
+        for (int i = 1; i <= N; i++) {
+		 if (existInDictionary(hashtag.substring(0,i) ,dictionary)){
+				System.out.println(hashtag.substring(0,i));	
+				breakHashTag(hashtag.substring(i), dictionary);
+			}
         }
     }
 
