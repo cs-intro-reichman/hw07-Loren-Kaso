b6e017cd3a42ff7ebe6f648b1c37d51dbb352b0e
loren.kaso <loren.kaso@post.runi.ac.il>
2024-02-04 17:17:21 +0200

    test all

--- SpellChecker.java
+++ SpellChecker.java
@@ -3,8 +3,8 @@ public class SpellChecker {
 
 
 	public static void main(String[] args) {
-		String word = "ulximaxe";
-		int threshold = 2;
+		String word = args[0];
+		int threshold = Integer.parseInt(args[1]);
 		String[] dictionary = readDictionary("dictionary.txt");
 		String correction = spellChecker(word, threshold, dictionary);
 		System.out.println(correction);
